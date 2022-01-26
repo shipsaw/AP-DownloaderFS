2e7849cdacf002e576345a82947e45633acd907d
Paul Kirby <kirby.paulh@gmail.com>
2022-01-25 21:58:53 -0500

    Added script to test HTTP login

--- ApDownloader.Core/HttpAccess.fs
+++ ApDownloader.Core/HttpAccess.fs
@@ -1,5 +1,25 @@
 module HttpAccess
 
+open System.Net
 
+let printWebsiteLogin =
+    let cookieContainer = CookieContainer()
 
+    let test =
+        Http.Request(
+            @"https://www.armstrongpowerhouse.com/index.php?route=account/login",
+            body =
+                FormValues [ "email", "USER_EMAIL"
+                             "password", "USER_PASSWORD" ]
+        )
 
+    test.Cookies
+    |> Map.iter (fun k v -> cookieContainer.Add(Cookie(k, v, "/", "www.armstrongpowerhouse.com")))
+
+    let downloads =
+        Http.RequestString(
+            @"https://www.armstrongpowerhouse.com/index.php?route=account/download",
+            cookieContainer = cookieContainer
+        )
+
+    printfn "%s" downloads
